012144f0bfb8f7a19b63ea53c050f055909697d0
fengdetian <1820938867@qq.com>
2024-12-10 16:20:02 +0800

    1.请求转发，调用成功后进行打印日志操作，由于Spring Cloud Gateway处理逻辑要等所有的过滤器执行完毕，再往下走，进行转发等操作，所以我们直接在过滤器中进行打印日志，调用次数+1等操作，会出现还未真正调用接口，就打印成功日志，调用次数+1，与我们的需求不符，所以我们使用了装饰者模式，给Response加入额外的功能，等接口调用完成后，调用writeWith方法，加入我们的逻辑。

--- src/main/java/com/fdt/CustomGlobalFilter.java
+++ src/main/java/com/fdt/CustomGlobalFilter.java
@@ -3,18 +3,26 @@ package com.fdt;
 import com.fdt.clientsdk.utils.SignUtils;
 import com.fdt.utils.RedisUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -55,7 +63,8 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         String sign = headers.getFirst("sign");
         String body = headers.getFirst("body");
         //校验用户标识 todo 接入后端平台后，应该查询数据库，获取用户标识
-        if(!accessKey.equals("fdt")){
+        // equals让不为null的字符串调用
+        if(!"fdt".equals(accessKey)){
             return handleNoAuth(response);
         }
         //随机数过期时间应该在时间戳过期之后，防止同时过期导致漏洞。
@@ -78,12 +87,10 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         // 5. 检测请求的接口是否存在
         // todo 接入后端平台后，应该查询数据库，接口是否存在，请求方法是否匹配，请求参数是否匹配
         // 6. **请求转发，调用接口**
-        // 7. 响应日志
-        // 8. 调用结果
-        //   - 调用成功，接口调用次数+1
-        //   - 调用失败，返回规范错误码
+//        Mono<Void> filter = chain.filter(exchange);
+        return handleResponse(exchange, chain);
 
-        return chain.filter(exchange);
+//        return filter;
     }
 
     @Override
@@ -91,10 +98,91 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         return -1;
     }
 
+    /**
+     * 处理响应
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    public Mono<Void> handleResponse(ServerWebExchange exchange, GatewayFilterChain chain) {
+        try {
+            // 获取原始的响应对象
+            ServerHttpResponse originalResponse = exchange.getResponse();
+            // 获取数据缓冲工厂
+            DataBufferFactory bufferFactory = originalResponse.bufferFactory();
+            // 获取响应的状态码
+            HttpStatus statusCode = originalResponse.getStatusCode();
+
+            // 判断状态码是否为200 OK(按道理来说,现在没有调用,是拿不到响应码的,对这个保持怀疑 沉思.jpg)
+            if(statusCode == HttpStatus.OK) {
+                // 创建一个装饰后的响应对象(开始穿装备，增强能力)
+                ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+
+                    // 重写writeWith方法，用于处理响应体的数据
+                    // 这段方法就是只要当我们的模拟接口调用完成之后,等它返回结果，
+                    // 就会调用writeWith方法,我们就能根据响应结果做一些自己的处理
+                    @Override
+                    public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                        log.info("body instanceof Flux: {}", (body instanceof Flux));
+                        // 判断响应体是否是Flux类型
+                        if (body instanceof Flux) {
+                            Flux<? extends DataBuffer> fluxBody = Flux.from(body);
+                            // 返回一个处理后的响应体
+                            // (这里就理解为它在拼接字符串,它把缓冲区的数据取出来，一点一点拼接好)
+                            return super.writeWith(fluxBody.map(dataBuffer -> {
+                                // todo 调用成功，接口调用次数+1
+                                // 读取响应体的内容并转换为字节数组
+                                byte[] content = new byte[dataBuffer.readableByteCount()];
+                                dataBuffer.read(content);
+                                DataBufferUtils.release(dataBuffer);//释放掉内存
+                                // 构建日志
+                                StringBuilder sb2 = new StringBuilder(200);
+                                //sb2.append("<--- {} {} \n");
+                                List<Object> rspArgs = new ArrayList<>();
+                                rspArgs.add(originalResponse.getStatusCode());
+                                //rspArgs.add(requestUrl);
+                                String data = new String(content, StandardCharsets.UTF_8);//data
+                                sb2.append(data);
+                                //log.info(sb2.toString(), rspArgs.toArray());//log.info("<-- {} {}\n", originalResponse.getStatusCode(), data);
+                                // 打印日志
+                                log.info("响应结果："+ data);
+                                // 将处理后的内容重新包装成DataBuffer并返回
+                                return bufferFactory.wrap(content);
+                            }));
+                        } else {
+                            // 调用失败，返回规范错误码
+                            log.error("<--- {} 响应code异常", getStatusCode());
+                        }
+                        return super.writeWith(body);
+                    }
+                };
+                // 对于200 OK的请求,将装饰后的响应对象传递给下一个过滤器链,并继续处理(设置repsonse对象为装饰过的)
+                return chain.filter(exchange.mutate().response(decoratedResponse).build());
+            }
+            // 对于非200 OK的请求，直接返回，进行降级处理
+            return chain.filter(exchange);
+        }catch (Exception e){
+            // 处理异常情况，记录错误日志
+            log.error("网关处理响应异常" + e);
+            return chain.filter(exchange);
+        }
+    }
+
+
+    // 拒绝访问，设置状态码方法
     public Mono<Void> handleNoAuth(ServerHttpResponse response){
         // 设置响应状态码为403，拒绝访问
         response.setStatusCode(HttpStatus.FORBIDDEN);
         // 返回处理完成的响应
         return response.setComplete();
     }
+
+    // 调用错误，设置状态码方法
+    public Mono<Void> handleInvokeError(ServerHttpResponse response){
+        // 设置响应状态码为500，调用失败
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        // 返回处理完成的响应
+        return response.setComplete();
+    }
+
 }
